2494e47d035d1ce830608315b824045998a3b954
SR-Rony <srrony707@gmail.com>
2025-06-02 22:26:41 +0600

    responsive nevbar add and article pages add

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div >
+    <div  className="py-80 text-center">
       <h1 className="text-5xl">Welcome to home pages</h1>
+      <Link className="mx-2 text-blue-600 hover:underline" href='articles/breaking-news-123?lang=en'>Read in English</Link>
+      <Link className="mx-2 text-blue-600 hover:underline" href='articles/breaking-news-123?lang=fr'>Read in French</Link>
+      <Link className="mx-2 text-blue-600 hover:underline" href='articles/breaking-news-123?lang=be'>Read in Bengla</Link>
     </div>
   );
 }
